dbbb6e738606770a48d493bb0469acdf237649bc
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-02-04 21:52:59 +0530

    Q4

--- DAY 4/Q4.c
+++ DAY 4/Q4.c
@@ -14,3 +14,36 @@ Output:
 Explanation: Swap pairs from both ends: (1,5), (2,4), middle 3 stays
 */
 
+#include<stdio.h>
+int main()
+{
+    int n, i, j, temp;
+    //Input number of elements
+    printf("Enter number of elements in an array: ");
+    scanf("%d", &n);
+    int arr[n];
+    //Input array elements
+    printf("Enter elements in the array: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    //Initialize start and end indexes
+    i=0;
+    j=n-1;
+    //Reverse array using swapping
+    while(i<j)
+    {
+        temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        i++;
+        j--;
+    }
+    //Display the updated array
+    for(i=0; i<n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
