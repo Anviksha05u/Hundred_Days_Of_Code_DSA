36ea195208f044f7f7a210b386858bf46562d7dc
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-02-05 23:27:24 +0530

    Q5

--- DAY 5/Q5.c
+++ DAY 5/Q5.c
@@ -18,3 +18,61 @@ Output:
 Explanation: Compare the next unprocessed arrival time from both logs and append the earlier one to the final log until all entries are processed
 */
 
+
+#include<stdio.h>
+int main()
+{
+    int p, q, i, j, k;
+    //Input number of entries in server log 1
+    printf("Enter number of entries in server log 1: ");
+    scanf("%d", &p);
+    int a[p];
+    //Input sorted integers
+    printf("Enter sorted integers for server log 1: ");
+    for(i=0; i<p; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    //Input number of entries in server log 2
+    printf("Enter number of entries in server log 2: ");
+    scanf("%d", &q);
+    int b[q];
+    //Input sorted integers
+    printf("Enter sorted integers for server log 2: ");
+    for(j=0; j<q; j++)
+    {
+        scanf("%d", &b[j]);
+    }
+    int c[p+q];
+    i = 0;
+    j = 0;
+    k = 0;
+    //Merge
+    while(i<p && j<q)
+    {
+        if(a[i]<b[j])
+        {
+            c[k++]=a[i++];
+        }
+        else
+        {
+            c[k++]=b[j++];
+        }
+    }
+    //Copying remaining elements of a[i] and b[j]
+    while(i<p)
+    {
+        c[k++]=a[i++];
+    }
+    while(j<q)
+    {
+        c[k++]=b[j++];
+    }
+    //Display the updated array
+    for(i=0; i<k; i++)
+    {
+        printf("%d ", c[i]);
+    }
+    printf("\n");
+    return 0;
+}
