006664bb115d8bc82ec062056d873807f6064ca9
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-02-06 16:30:56 +0530

    Q6

--- DAY 6/Q6.c
+++ DAY 6/Q6.c
@@ -13,5 +13,36 @@ Output:
 1 2 3
 Explanation: Keep first occurrence of each element: 1, 2, 3
 */
-  
-  
+
+#include<stdio.h>
+int main()
+{
+    int n, i;
+    //Input number of elements
+    printf("Enter number of elements in an array: ");
+    scanf("%d", &n);
+    int arr[n];
+    //Input array elements
+    printf("Enter elements in the array: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    //To store the index of last unique element
+    int j=0;
+    //Traverse array from second element
+    for(i=1; i<n; i++)
+    {
+        if(arr[i]!=arr[j])
+        {
+            j++;
+            arr[j]=arr[i];
+        }
+    }
+    //Display the updated array
+    for(i=0; i<=j; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
