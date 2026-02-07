8d0f262367aab3d7151a77177c2bb83c6537b431
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-02-07 23:14:37 +0530

    Q7

--- DAY 7/Q7.c
+++ DAY 7/Q7.c
@@ -11,3 +11,26 @@ Output:
 8
 Explanation: Sequence: 0,1,1,2,3,5,8 at positions 0,1,2,3,4,5,6
 */
+
+#include <stdio.h>
+//Recursive function to find fibonacci number
+int fib(int n)
+{
+    //Base case
+    if(n==0)
+    return 0;
+    if(n==1)
+    return 1;
+    //Recursive case
+    return fib(n-1)+fib(n-2);
+}
+int main()
+{
+    int n;
+    //Input a number
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    //Display result
+    printf("%d", fib(n));
+    return 0;
+}
