0d0ce0805f67a709feaffa337e7915a71f5b9da7
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-02-10 21:55:46 +0530

    Q10

--- DAY 10/Q10.c
+++ DAY 10/Q10.c
@@ -12,3 +12,33 @@ YES
 Explanation: String reads same forwards and backwards
 */
 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    //Input a string
+    printf("Enter a string: ");
+    scanf("%s", str);
+    int i=0;
+    int j=strlen(str)-1;
+    int isPalindrome=1;
+    //Compare characters from both ends 
+    while(i<j)
+    {
+        
+        if(str[i]!=str[j])
+        {
+            isPalindrome=0;
+            break;
+        }
+        i++;
+        j--;
+    }
+    //Display result
+    if(isPalindrome)
+    printf("YES\n");
+    else
+    printf("NO\n");
+    return 0;
+}
