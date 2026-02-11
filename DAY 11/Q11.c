36d48d27600321a875125054713c7a267592c09b
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-02-11 14:38:59 +0530

    Q11

--- DAY 11/Q11.c
+++ DAY 11/Q11.c
@@ -54,3 +54,48 @@ Output:
 9
 */
 
+#include<stdio.h>
+int main()
+{
+    int m, n;
+    //Input number of rows and columns
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+    int a[m][n], b[m][n], sum[m][n];
+    //Input first matrix elements
+    printf("Enter elements: ");
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    //Input second matrix elements
+    printf("Enter elements: ");
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    //Add both matrices
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            sum[i][j]=a[i][j]+b[i][j];
+        }
+    }
+    //Print resultant matrix
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
