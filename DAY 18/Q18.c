498a123d894f7509359b4e142a24dfff358718ad
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-02-19 22:00:20 +0530

    Q18

--- DAY 18/Q18.c
+++ DAY 18/Q18.c
@@ -15,3 +15,46 @@ Output:
 4 5 1 2 3
 */
 
+#include <stdio.h>
+
+int main() 
+{
+    int n, k;
+    //Read size of array
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    //Read array elements
+    printf("Enter elements: ");
+    for(int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    //Read number of rotations
+    printf("Enter number of rotations: ");
+    scanf("%d", &k);
+    //If k is greater than n, reduce it
+    k = k % n;
+    int temp[n];
+    //Copy last k elements of arr into the beginning of temp
+    for(int i = 0; i < k; i++) 
+    {
+        temp[i] = arr[n - k + i];
+    }
+    //Copy remaining elements of arr into temp after the last k elements
+    for(int i = 0; i < n - k; i++) 
+    {
+        temp[k + i] = arr[i];
+    }
+    //Copy rotated elements back into original array
+    for(int i = 0; i < n; i++) 
+    {
+        arr[i] = temp[i];
+    }
+    //Print rotated array
+    for(int i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
