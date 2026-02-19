004f3145bee9866a8b22fd76b3129d0c92271d22
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-02-19 22:06:29 +0530

    Q19

--- DAY 19/Q19.c
+++ DAY 19/Q19.c
@@ -14,3 +14,39 @@ Output:
 Explanation: Among all possible pairs, the sum of -10 and 1 is -9, which is the closest to zero compared to other pairs.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+int main() 
+{
+    int n;
+    //Read size of array
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements: ");
+    //Read array elements
+    for(int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    int minSum = abs(arr[0] + arr[1]);
+    int x = arr[0], y = arr[1];
+    //Check all pairs
+    for(int i = 0; i < n; i++) 
+    {
+        for(int j = i + 1; j < n; j++) 
+        {
+            int sum = abs(arr[i] + arr[j]);
+            //Update minimum sum if current pair is closer to zero
+            if(sum < minSum) 
+            {
+                minSum = sum;
+                x = arr[i];
+                y = arr[j];
+            }
+        }
+    }
+    //Print the result
+    printf("%d %d", y, x);
+    return 0;
+}
