c76aadbaab808090bef5fc7f8515d874939f053e
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-02-22 21:54:36 +0530

    Q22

--- DAY 22/Q22.c
+++ DAY 22/Q22.c
@@ -13,3 +13,52 @@ Output:
 10 20 30 40 50
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+//Structure for linked list node
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+int main()
+{
+    int n;
+    //Read number of nodes
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    struct Node *head = NULL;
+    struct Node *temp = NULL;
+    struct Node *newNode = NULL;
+    int value;
+    //Create linked list
+    printf("Enter value: ");
+    for(int i = 0; i < n; i++)
+    {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &value);
+        newNode->data = value;
+        newNode->next = NULL;
+        if(head == NULL)
+        {
+            head = newNode; //First node
+        }
+        else
+        {
+            temp->next = newNode; //Link new node
+        }
+
+        temp = newNode; //Move temp forward
+    }
+    //Count nodes
+    int count = 0;
+    temp = head;
+    while(temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    //Print result
+    printf("%d", count);
+    return 0;
+}
