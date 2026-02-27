416c2f498b60226610574328911f8b4fea47a344
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-02-27 20:39:54 +0530

    Q26

--- DAY 26/Q26.c
+++ DAY 26/Q26.c
@@ -15,3 +15,61 @@ Explanation:
 Each node has data, next, prev. Insert nodes sequentially, traverse from head using next pointer.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+//Structure for Doubly Linked List node
+struct Node
+{
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+//Function to create a new node
+struct Node* createNode(int data)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+//Function to print list in forward direction
+void printList(struct Node* head)
+{
+    while (head != NULL)
+    {
+        printf("%d", head->data);
+        if (head->next != NULL)
+            printf(" ");
+        head = head->next;
+    }
+}
+int main()
+{
+    int n, value;
+    struct Node *head = NULL, *tail = NULL;
+    //Read number of nodes
+    printf("Ente number of nodes: ");
+    scanf("%d", &n);
+    //Insert nodes sequentially
+    printf("Ente value: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+        struct Node* newNode = createNode(value);
+        if (head == NULL)
+        {
+            head = tail = newNode; //First node
+        }
+        else
+        {
+            tail->next = newNode; //Connect next
+            newNode->prev = tail; //Connect previous
+            tail = newNode; //Move tail forward
+        }
+    }
+    //Traverse in forward direction
+    printList(head);
+    return 0;
+}
