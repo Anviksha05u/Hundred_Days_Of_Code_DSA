acac5690cbb712386747833326a2333a78bbb63f
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-03-04 15:24:37 +0530

    Q32

--- DAY 32/Q32.c
+++ DAY 32/Q32.c
@@ -15,3 +15,69 @@ Output:
 30 20 10
 */
 
+#include <stdio.h>
+#define MAX 100
+int stack[MAX];
+int top=-1;
+//Function to push value onto stack
+void push(int val) 
+{
+    if (top==MAX - 1) 
+    {
+        printf("Stack Overflow\n");
+    } 
+    else 
+    {
+        top++;
+        stack[top]=val;
+    }
+}
+//Function to pop top value from stack
+void pop() 
+{
+    if (top==-1) 
+    {
+        printf("Stack Underflow\n");
+    } 
+    else 
+    {
+        top--;
+    }
+}
+//Function to display stack from top to bottom
+void display() 
+{
+    if (top==-1) 
+    {
+        printf("Stack is empty\n");
+    } 
+    else 
+    {
+        for (int i=top; i>=0; i--) 
+        {
+            printf("%d", stack[i]);
+            if (i!=0) printf(" ");
+        }
+        printf("\n");
+    }
+}
+int main() 
+{
+    int n, m, val;
+    printf("Enter number of elements to push: ");
+    scanf("%d", &n); //Number of elements to push
+    printf("Enter value: ");
+    for (int i=0; i<n; i++) 
+    {
+        scanf("%d", &val);
+        push(val);
+    }
+    printf("Enter number of pops: ");
+    scanf("%d", &m); //Number of pops
+    for (int i=0; i<m; i++) 
+    {
+        pop();
+    }
+    display(); //Print remaining elements
+    return 0;
+}
