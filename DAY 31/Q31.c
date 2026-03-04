68254f28569376424f0c7c47dd270cf5946be7c4
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-03-04 15:16:43 +0530

    Q31

--- DAY 31/Q31.c
+++ DAY 31/Q31.c
@@ -23,3 +23,77 @@ Output:
 10
 */
 
+#include <stdio.h>
+#define MAX 100
+int stack[MAX];
+int top=-1;
+//Function to push element into stack
+void push(int val) 
+{
+    if (top==MAX - 1) 
+    {
+        printf("Stack Overflow\n");
+    } 
+    else 
+    {
+        top++;
+        stack[top]=val;
+    }
+}
+//Function to pop element from stack
+void pop() 
+{
+    if (top==-1) 
+    {
+        printf("Stack Underflow\n");
+    } 
+    else 
+    {
+        printf("%d\n", stack[top]);
+        top--;
+    }
+}
+//Function to display stack elements from top to bottom
+void display() 
+{
+    if (top==-1) 
+    {
+        printf("Stack is empty\n");
+    } 
+    else 
+    {
+        for (int i=top; i>=0; i--) 
+        {
+            printf("%d", stack[i]);
+            if (i!=0) printf(" ");
+        }
+        printf("\n");
+    }
+}
+int main() 
+{
+  int n;
+  printf("Enter number of operations: ");
+  scanf("%d", &n); //Number of operations
+  for (int i=0; i<n; i++) 
+  {
+    int op, val;
+    printf("Enter operation: ");
+    scanf("%d", &op);
+    if (op==1) //Push operation
+    {
+      printf("Enter value: ");
+      scanf("%d", &val);
+      push(val); 
+    } 
+    else if (op==2) //Pop operation
+    {   
+      pop(); 
+    } 
+    else if (op==3) //Display
+    {   
+      display(); 
+    }
+  }
+  return 0;
+}
