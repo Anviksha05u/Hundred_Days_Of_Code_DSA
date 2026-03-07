dba66cd3b56d0c209d894934b27fcd5dbf644490
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-03-07 10:23:27 +0530

    Q34

--- DAY 34/Q34.c
+++ DAY 34/Q34.c
@@ -13,3 +13,75 @@ Explanation:
 Use stack to store operands, apply operators by popping operands, push result back. Final stack top is result.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+//Node structure for stack
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+//Push operation
+void push(struct Node** top, int value)
+{
+    struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
+    newNode->data=value;
+    newNode->next=*top;
+    *top=newNode;
+}
+//Pop operation
+int pop(struct Node** top)
+{
+    if (*top==NULL)
+    {
+        printf("Stack Underflow\n");
+        return 0;
+    }
+    struct Node* temp=*top;
+    int value=temp->data;
+    *top=temp->next;
+    free(temp);
+    return value;
+}
+int main()
+{
+    struct Node* stack = NULL;
+    char expr[100];
+    printf("Enter postfix expression: ");
+    fgets(expr, sizeof(expr), stdin);
+    char* token=strtok(expr, " ");
+
+    while (token!=NULL)
+    {
+        //If operand → push
+        if (isdigit(token[0]) || 
+           (token[0]=='-' && isdigit(token[1])))
+        {
+            push(&stack, atoi(token));
+        }
+        else
+        {
+            //Pop two operands
+            int b=pop(&stack);
+            int a=pop(&stack);
+            int result;
+            //Apply operator
+            if (token[0]=='+')
+                result=a + b;
+            else if (token[0]=='-')
+                result=a - b;
+            else if (token[0]=='*')
+                result=a * b;
+            else if (token[0]=='/')
+                result=a / b;
+            //Push result
+            push(&stack, result);
+        }
+        token=strtok(NULL, " ");
+    }
+    //Final result
+    printf("%d\n", pop(&stack));
+    return 0;
+}
