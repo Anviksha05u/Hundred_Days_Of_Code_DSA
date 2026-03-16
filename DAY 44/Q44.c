08082b34dde61578c81170f2986154dfea4b5321
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-03-16 23:36:56 +0530

    Q44

--- DAY 44/Q44.c
+++ DAY 44/Q44.c
@@ -16,3 +16,76 @@ Output:
 4 5 2 6 7 3 1
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *left,*right;
+};
+struct Node* createNode(int val)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+void inorder(struct Node* root)
+{
+    if(root==NULL) return;
+    inorder(root->left);
+    printf("%d ",root->data);
+    inorder(root->right);
+}
+void preorder(struct Node* root)
+{
+    if(root==NULL) return;
+    printf("%d ",root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(struct Node* root)
+{
+    if(root==NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ",root->data);
+}
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter elements: ");
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    if(arr[0]==-1) return 0;
+    struct Node* root = createNode(arr[0]);
+    struct Node* queue[n];
+    int front=0,rear=0;
+    queue[rear++] = root;
+    int i=1;
+    while(i<n)
+    {
+        struct Node* curr = queue[front++];
+        if(arr[i]!=-1)
+        {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+        if(i<n && arr[i]!=-1)
+        {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+    inorder(root);
+    printf("\n");
+    preorder(root);
+    printf("\n");
+    postorder(root);
+    return 0;
+}
