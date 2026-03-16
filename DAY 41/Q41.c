02e0af3da3a41e27b66f6de92d95951dc84b7eba
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-03-16 23:21:18 +0530

    Q41

--- DAY 41/Q41.c
+++ DAY 41/Q41.c
@@ -9,3 +9,61 @@ Output Format:
 - Print -1 if dequeue is attempted on an empty queue
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+struct Node* front = NULL;
+struct Node* rear = NULL;
+void enqueue(int x)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->next = NULL;
+    if(rear == NULL)
+    {
+        front = rear = newNode;
+        return;
+    }
+    rear->next = newNode;
+    rear = newNode;
+}
+int dequeue()
+{
+    if(front == NULL)
+        return -1;
+    struct Node* temp = front;
+    int val = temp->data;
+    front = front->next;
+    if(front == NULL)
+        rear = NULL;
+    free(temp);
+    return val;
+}
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter operations: ");
+    while(n--)
+    {
+        char op[20];
+        scanf("%s", op);
+        if(strcmp(op, "enqueue") == 0)
+        {
+            int x;
+            scanf("%d", &x);
+            enqueue(x);
+        }
+        else if(strcmp(op, "dequeue") == 0)
+        {
+            printf("%d\n", dequeue());
+        }
+    }
+    return 0;
+}
