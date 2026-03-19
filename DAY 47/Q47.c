f46ef8c40fa503c8e7ab0481796bbcbba0eadad1
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-03-19 12:19:27 +0530

    Q47

--- DAY 47/Q47.c
+++ DAY 47/Q47.c
@@ -7,3 +7,64 @@ Output:
 - Output specifications
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+struct Node 
+{
+    int data;
+    struct Node *left, *right;
+};
+struct Node* createNode(int val)
+{
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+int height(struct Node* root)
+{
+    if(root == NULL)
+        return 0;
+    int left = height(root->left);
+    int right = height(root->right);
+    return (left > right ? left : right) + 1;
+}
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements: ");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    if(arr[0] == -1)
+    {
+        printf("0");
+        return 0;
+    }
+    struct Node* root = createNode(arr[0]);
+    struct Node* queue[n];
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+    int i = 1;
+    //Build tree
+    while(i < n)
+    {
+        struct Node* curr = queue[front++];
+        if(arr[i] != -1)
+        {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+        if(i < n && arr[i] != -1)
+        {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+    printf("%d", height(root));
+    return 0;
+}
