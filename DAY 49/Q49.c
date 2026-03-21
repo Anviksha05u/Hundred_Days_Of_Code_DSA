caa0fb7dffffb77e099ad5fd9fe5372c45996a42
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-03-21 11:25:52 +0530

    Q49

--- DAY 49/Q49.c
+++ DAY 49/Q49.c
@@ -7,3 +7,50 @@ Output:
 - Output specifications
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+struct Node \
+{
+    int data;
+    struct Node *left, *right;
+};
+struct Node* createNode(int val)
+{
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+struct Node* insert(struct Node* root, int val)
+{
+    if(root == NULL)
+        return createNode(val);    if(val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val); 
+    return root;
+}
+void inorder(struct Node* root)
+{
+    if(root == NULL)
+    return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    struct Node* root = NULL;
+    printf("Enter elements: ");
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+    inorder(root);
+    return 0;
+}
