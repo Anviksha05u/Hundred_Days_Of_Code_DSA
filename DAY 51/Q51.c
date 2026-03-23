c9105ee5d2ddb2d6b9a1ac51740ea60a9bcd2f6c
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-03-23 22:04:55 +0530

    Q51

--- DAY 51/Q51.c
+++ DAY 51/Q51.c
@@ -16,3 +16,68 @@ Output:
 6
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+//Tree Node
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+//Create new node
+struct Node* createNode(int val)
+{
+    struct Node* node=(struct Node*)malloc(sizeof(struct Node));
+    node->data=val;
+    node->left=node->right=NULL;
+    return node;
+}
+//Insert into BST
+struct Node* insert(struct Node* root, int val)
+{
+    if(root==NULL)
+        return createNode(val);
+    if(val<root->data)
+        root->left=insert(root->left, val);
+    else
+        root->right=insert(root->right, val);
+    return root;
+}
+//Find LCA in BST
+struct Node* findLCA(struct Node* root, int n1, int n2) 
+{
+    while(root!=NULL) 
+    {
+        //Both nodes in left subtree
+        if(n1<root->data && n2<root->data)
+            root=root->left;
+        //Both nodes in right subtree
+        else if(n1>root->data && n2>root->data)
+            root=root->right;
+        //Split point → LCA found
+        else
+            return root;
+    }
+    return NULL;
+}
+int main()
+{
+    int n, x, n1, n2;
+    struct Node* root=NULL;
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    printf("Enter elements: ");
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &x);
+        root=insert(root, x);
+    }
+    printf("Enter two node values: ");
+    scanf("%d %d", &n1, &n2);
+    struct Node* lca=findLCA(root, n1, n2);
+    if(lca!=NULL)
+        printf("LCA: %d\n", lca->data);
+    else
+        printf("LCA not found\n");
+    return 0;
+}
