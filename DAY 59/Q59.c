88232ad0756fa534fb63c5f7b0a07a1a907fb17e
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-03-31 19:39:21 +0530

    Q59

--- DAY 59/Q59.c
+++ DAY 59/Q59.c
@@ -18,3 +18,75 @@ Explanation:
 Postorder gives root at end, inorder divides left and right subtrees.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+//Tree node
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+//Create node
+struct Node* createNode(int val)
+{
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+//Find index in inorder
+int findIndex(int inorder[], int start, int end, int val)
+{
+    for(int i = start; i <= end; i++)
+    {
+        if(inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+//Build tree
+struct Node* buildTree(int inorder[], int postorder[], int start, int end, int* postIndex)
+{
+    if(start > end)
+        return NULL;
+    //Pick root from postorder
+    int val = postorder[*postIndex];
+    (*postIndex)--;
+    struct Node* root = createNode(val);
+    //If leaf node
+    if(start == end)
+        return root;
+    int index = findIndex(inorder, start, end, val);
+    //Build RIGHT first (important!)
+    root->right = buildTree(inorder, postorder, index + 1, end, postIndex);
+    root->left  = buildTree(inorder, postorder, start, index - 1, postIndex);
+
+    return root;
+}
+//Preorder traversal
+void preorder(struct Node* root)
+{
+    if(root == NULL)
+        return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+int main()
+{
+    int n;
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    int inorder[n], postorder[n];
+    printf("Enter inorder: ");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &inorder[i]);
+    printf("Enter postorder: ");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &postorder[i]);
+    int postIndex = n - 1;
+    struct Node* root = buildTree(inorder, postorder, 0, n - 1, &postIndex);
+    printf("Preorder: ");
+    preorder(root);
+    return 0;
+}
