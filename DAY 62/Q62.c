e1f6402dd7769b3dd334fa6ccf5257f53f30899d
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-04-03 23:54:31 +0530

    Q62.c

---
+++ DAY 62/Q62.c
@@ -0,0 +1,10 @@
+/*
+Problem: Build a graph with adjacency list representation. Use linked lists or dynamic arrays.
+Input:
+- n (vertices)
+- m (edges)
+- edges (u, v)
+Output:
+- List of adjacency lists for each vertex
+*/
+
