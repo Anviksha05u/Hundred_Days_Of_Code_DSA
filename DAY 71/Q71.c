fc266c4c39118ee996352b3e82a5a2141ed638a3
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-04-12 18:41:51 +0530

    Q71

--- DAY 71/Q71.c
+++ DAY 71/Q71.c
@@ -20,3 +20,67 @@ Explanation
 Collisions resolved using i² jumps.
 */
 
+#include <stdio.h>
+#define EMPTY -1
+int main()
+{
+    int m, q;
+    printf("Enter size of hash table: ");
+    scanf("%d", &m);
+    int table[m];
+    //Initialize table
+    for (int i = 0; i < m; i++)
+    {
+        table[i] = EMPTY;
+    }
+    printf("Enter number of operations: ");
+    scanf("%d", &q);
+    for (int i = 0; i < q; i++)
+    {
+        char op[10];
+        int key;
+        scanf("%s %d", op, &key);
+        //INSERT operation
+        if (op[0] == 'I')
+        {
+            int h = key % m;
+            int j = 0;
+            while (j < m)
+            {
+                int index = (h + j * j) % m;
+                if (table[index] == EMPTY)
+                {
+                    table[index] = key;
+                    break;
+                }
+                j++;
+            }
+        }
+        //SEARCH operation
+        else if (op[0] == 'S')
+        {
+            int h = key % m;
+            int j = 0;
+            int found = 0;
+            while (j < m)
+            {
+                int index = (h + j * j) % m;
+                if (table[index] == key)
+                {
+                    found = 1;
+                    break;
+                }
+                if (table[index] == EMPTY)
+                {
+                    break;
+                }
+                j++;
+            }
+            if (found)
+                printf("FOUND\n");
+            else
+                printf("NOT FOUND\n");
+        }
+    }
+    return 0;
+}
