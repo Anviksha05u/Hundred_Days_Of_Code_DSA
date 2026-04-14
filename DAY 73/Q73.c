1861080d82ab91db1bbf3a9e3b8ebfd1efa354d9
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-04-14 11:24:11 +0530

    Q73

--- DAY 73/Q73.c
+++ DAY 73/Q73.c
@@ -13,3 +13,28 @@ Explanation
 The character 'f' occurs only once in the string and appears before any other non-repeating character.
 */
 
+#include <stdio.h>
+int main()
+{
+    char s[1000];
+    int freq[26] = {0};
+    printf("Enter a string: ");
+    scanf("%s", s);
+    //Count frequency of each character
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        freq[s[i] - 'a']++;
+    }
+    //Find first character with frequency 1
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (freq[s[i] - 'a'] == 1)
+        {
+            printf("%c\n", s[i]);
+            return 0;
+        }
+    }
+    //If none found
+    printf("$\n");
+    return 0;
+}
