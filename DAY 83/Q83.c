3673f94520a9ebdab48cfebb73dddb2da5065921
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-04-24 21:02:07 +0530

    Q83

--- DAY 83/Q83.c
+++ DAY 83/Q83.c
@@ -13,3 +13,37 @@ Output:
 12 22 25 34 64
 */
 
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    //Input array
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    //Selection Sort
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        //Swap
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    //Output
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
