94be607bac965779cb2974f388bd4a252306f978
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-04-29 11:54:02 +0530

    Q88

--- DAY 88/Q88.c
+++ DAY 88/Q88.c
@@ -16,3 +16,55 @@ Explanation
 Cows can be placed at positions 1, 4, and 8. The minimum distance between any two cows is 3, which is the maximum possible.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+//Comparator for sorting
+int compare(const void* a, const void* b)
+{
+    return (*(int*)a - *(int*)b);
+}
+//Check if cows can be placed with minimum distance = dist
+int canPlace(int stalls[], int n, int k, int dist)
+{
+    int count = 1; //First cow at first stall
+    int last = stalls[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (stalls[i] - last >= dist)
+        {
+            count++;
+            last = stalls[i];
+        }
+        if (count >= k)
+            return 1;
+    }
+    return 0;
+}
+int main()
+{
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int stalls[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &stalls[i]);
+    //Sort stalls
+    qsort(stalls, n, sizeof(int), compare);
+    int low = 1;
+    int high = stalls[n - 1] - stalls[0];
+    int ans = 0;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (canPlace(stalls, n, k, mid))
+        {
+            ans = mid;
+            low = mid + 1; // try bigger distance
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    printf("%d", ans);
+    return 0;
+}
