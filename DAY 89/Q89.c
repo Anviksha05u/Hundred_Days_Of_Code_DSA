b01638c97bf2641d450215a3ea180a41c8b04c47
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-04-30 23:35:36 +0530

    Q89

--- DAY 89/Q89.c
+++ DAY 89/Q89.c
@@ -19,3 +19,55 @@ Student 2: 90
 Maximum pages = 113 (minimum possible).
 */
 
+#include <stdio.h>
+//Check if allocation is possible
+int canAllocate(int arr[], int n, int m, int maxPages)
+{
+    int students = 1;
+    int pages = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (pages + arr[i] <= maxPages)
+        {
+            pages += arr[i];
+        }
+        else
+        {
+            students++;
+            pages = arr[i];
+            if (students > m)
+                return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int arr[n];
+    int sum = 0, maxVal = 0;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+        if (arr[i] > maxVal)
+            maxVal = arr[i];
+    }
+    int low = maxVal;
+    int high = sum;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (canAllocate(arr, n, m, mid))
+        {
+            high = mid - 1;  // try better (smaller max)
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    printf("%d", low);
+    return 0;
+}
