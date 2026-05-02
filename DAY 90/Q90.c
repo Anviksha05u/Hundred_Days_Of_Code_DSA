95b9d26ef7709c9ac05760ffc9162a61857bb5cf
Anviksha <Anviksha.26976@stu.upes.ac.in>
2026-05-02 10:28:08 +0530

    Q90

--- DAY 90/Q90.c
+++ DAY 90/Q90.c
@@ -16,3 +16,55 @@ Explanation
 One painter paints boards of length 10, 20, and 30 (total 60), while the other paints board of length 40.
 */
 
+#include <stdio.h>
+//Check if allocation is possible within maxTime
+int canPaint(int boards[], int n, int k, int maxTime)
+{
+    int painters = 1;
+    int time = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (time + boards[i] <= maxTime)
+        {
+            time += boards[i];
+        }
+        else
+        {
+            painters++;
+            time = boards[i];
+            if (painters > k)
+                return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int boards[n];
+    int sum = 0, maxVal = 0;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &boards[i]);
+        sum += boards[i];
+        if (boards[i] > maxVal)
+            maxVal = boards[i];
+    }
+    int low = maxVal;
+    int high = sum;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (canPaint(boards, n, k, mid))
+        {
+            high = mid - 1; //Try better (less time)
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    printf("%d", low);
+    return 0;
+}
